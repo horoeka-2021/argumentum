006dddf0e4ce94e459f7213ceb4701b86e84bd11
Angus McConnell <angus.arnot.mcconnell@gmail.com>
2021-12-03 12:37:21 +1300

    implement clientside auth-0

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import { cacheUser } from '../auth0-utils'
+import { useAuth0 } from '@auth0/auth0-react'
 // import ErrorMessage from './ErrorMessage.jsx'
 
 // import Components
 import ExampleComponent from './ExampleComponent.jsx'
 
 function App () {
+  cacheUser(useAuth0)
   return (
     <div className='app'>
       <Route path='/' component={ExampleComponent} />
--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 
 // import reducers
-import exampleReducer from './exampleReducer'
+import user from './user'
 
 export default combineReducers({
-  exampleReducer
+  user
 })
