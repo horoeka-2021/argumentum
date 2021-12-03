3bf141660a33bf0906412d1391707af30104e4cf
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-12-04 12:22:09 +1300

    Update migrations

--- server/db/migrations/20211203035035_userArguments.js
+++ server/db/migrations/20211203035035_userArguments.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('userArguments', (table) => {
-    table.integer('user_auth0_id').references('users.auth0_id')
+    table.string('user_auth0_id').references('users.auth0_id')
     table.integer('argument_id').references('arguments.id')
     table.string('side')
   })
