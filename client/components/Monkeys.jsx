a5f0ac9d24a095eddbc215fd1452df2d94142dae
tane-git <tanelives@gmail.com>
2021-12-07 11:15:54 +1300

    add effect to selected monkey

--- client/components/Monkeys.jsx
+++ client/components/Monkeys.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Table from 'react-bootstrap/Table'
 import { useDispatch } from 'react-redux'
 import { monkeyAction } from '../actions/monkey'
@@ -6,8 +6,17 @@ import { monkeyAction } from '../actions/monkey'
 export default function Monkeys () {
   const dispatch = useDispatch()
 
+  const [monkey, setMonkey] = useState('')
+
   function handleClick (i) {
+    setMonkey(i)
     dispatch(monkeyAction(i))
+  }
+
+  function selected (i) {
+    return i === monkey
+      ? 'tableMonkey selectedMonkey'
+      : 'tableMonkey'
   }
 
   const numbers = Array.from({ length: 9 }, (v, i) => ({ src: `images/monkeys/${i + 1}.jpg`, i }))
@@ -66,19 +75,19 @@ export default function Monkeys () {
         {/* Generate a table using a map of the numbers array, and a ternary operator, where after every 3rd element, the current row is tag is closed, and a new row is started */}
         <tbody>
           <tr>
-            <td><img className='tableMonkey' onClick={() => handleClick(1)} src={'images/monkeys/1.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(2)} src={'images/monkeys/2.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(3)} src={'images/monkeys/3.jpg'} /></td>
+            <td><img className={selected(1)} onClick={() => handleClick(1)} src={'images/monkeys/1.jpg'} /></td>
+            <td><img className={selected(2)} onClick={() => handleClick(2)} src={'images/monkeys/2.jpg'} /></td>
+            <td><img className={selected(3)} onClick={() => handleClick(3)} src={'images/monkeys/3.jpg'} /></td>
           </tr>
           <tr>
-            <td><img className='tableMonkey' onClick={() => handleClick(4)} src={'images/monkeys/4.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(5)} src={'images/monkeys/5.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(6)} src={'images/monkeys/6.jpg'} /></td>
+            <td><img className={selected(4)} onClick={() => handleClick(4)} src={'images/monkeys/4.jpg'} /></td>
+            <td><img className={selected(5)} onClick={() => handleClick(5)} src={'images/monkeys/5.jpg'} /></td>
+            <td><img className={selected(6)} onClick={() => handleClick(6)} src={'images/monkeys/6.jpg'} /></td>
           </tr>
           <tr>
-            <td><img className='tableMonkey' onClick={() => handleClick(7)} src={'images/monkeys/7.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(8)} src={'images/monkeys/8.jpg'} /></td>
-            <td><img className='tableMonkey' onClick={() => handleClick(9)} src={'images/monkeys/9.jpg'} /></td>
+            <td><img className={selected(7)} onClick={() => handleClick(7)} src={'images/monkeys/7.jpg'} /></td>
+            <td><img className={selected(8)} onClick={() => handleClick(8)} src={'images/monkeys/8.jpg'} /></td>
+            <td><img className={selected(9)} onClick={() => handleClick(9)} src={'images/monkeys/9.jpg'} /></td>
           </tr>
         </tbody>
       </Table>
