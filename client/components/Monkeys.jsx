dd2c08239653af6338a9762b6f92c65d35c9ae45
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-12-07 16:45:10 +1300

    Update Monkeys image max-width

--- client/components/Monkeys.jsx
+++ client/components/Monkeys.jsx
@@ -15,63 +15,13 @@ export default function Monkeys () {
 
   function selected (i) {
     return i === monkey
-      ? 'tableMonkey selectedMonkey'
-      : 'tableMonkey'
+      ? 'tableMonkey selectedMonkey monkey-img'
+      : 'tableMonkey monkey-img'
   }
-
-  const numbers = Array.from({ length: 9 }, (v, i) => ({ src: `images/monkeys/${i + 1}.jpg`, i }))
-  const chunks = chunk(numbers, 3)
 
   return (
     <div className='Monkeys'>
-      <Table striped borderless hover>
-        {/* Ahmads code, need to check if this works then refactored ours to match */}
-        {/* <tbody>
-          {chunks.map((row, i) =>
-            <tr key={i}>
-              {row.map(monkey =>
-                <td key={monkey.i}>
-                  <img
-                    width='100px'
-                    src={monkey.src}
-                    onClick={() => handleClick(monkey.i)}
-                  />
-                </td>)}
-            </tr>
-          )}
-        </tbody> */}
-        {/* <tbody>
-          <tr>
-            <td>
-              {numbers.map(image => <img key={image.i} className='tableMonkey' onClick={() => handleClick(image.i)} src={image.src} />)}
-            </td>
-          </tr>
-        </tbody> */}
-
-        {/* // ? <img key={image.i} className='tableMonkey' onClick={() => handleClick(image.i)} src={image.src} /> */}
-
-        {/* Trying to generate table programitcally... */}
-        {/* <tbody>
-          <tr>
-            {numbers.map(image =>
-              ((image.i + 1) % 3 === 0)
-                ? </tr><tr><td>n</td>
-                : <td>t</td>
-            )}
-          </tr>
-        </tbody> */}
-
-        {/* Generate a 3x3 table programitcally using a map of the numbers array, starting a new row every 3rd element */}
-        {/* <tbody>
-          <tr>
-            {numbers.map(image =>
-              ((image.i + 1) % 3 === 0)
-                ? </tr><tr><td><img className='tableMonkey' onClick={() => handleClick(image.i)} src={image.src} /></td>
-                : <td><img className='tableMonkey' onClick={() => handleClick(image.i + 1)} src={image.src} /></td>
-            )}
-          </tr>
-        </tbody> */}
-
+      <Table responsive striped borderless hover>
         {/* Generate a table using a map of the numbers array, and a ternary operator, where after every 3rd element, the current row is tag is closed, and a new row is started */}
         <tbody>
           <tr>
@@ -94,11 +44,3 @@ export default function Monkeys () {
     </div>
   )
 }
-
-const chunk = (arr, size) =>
-  arr
-    .reduce((acc, _, i) =>
-      (i % size)
-        ? acc
-        : [...acc, arr.slice(i, i + size)]
-    , [])
