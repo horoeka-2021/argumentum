81ebdf24e6619126460f2a50147846bfbbd12573
tane-git <tanelives@gmail.com>
2021-12-08 19:56:53 +1300

    add myself to database to avoid errors in not having any seeds

--- server/db/seeds/userArguments.js
+++ server/db/seeds/userArguments.js
@@ -4,6 +4,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('user_arguments').insert([
+        { user_id: 'auth0|61b00fe4013f920068a29725', arg_id: 1, side: 'No', story: 'God spoke to me once and told me he was made up' }
       ])
     })
 }
--- server/db/seeds/users.js
+++ server/db/seeds/users.js
@@ -4,6 +4,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
+        { auth0_id: 'auth0|61b00fe4013f920068a29725', email: 'tanelives@gmail.com', image: 9, username: 'xkeyboardWarrior' }
       ])
     })
 }
