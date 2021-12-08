c67d6823c41feebc75e8bac65b782a09984ccba3
James Wallace <4538317+NZLWally@users.noreply.github.com>
2021-12-08 22:09:42 +1300

    Add primary key to user_arguments

--- server/db/migrations/3_user_arguments.js
+++ server/db/migrations/3_user_arguments.js
@@ -1,5 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('user_arguments', (table) => {
+    table.increments('id').primary()
     table.string('user_id').references('users.auth0_id')
     table.integer('arg_id').references('arguments.id')
     table.string('side')
