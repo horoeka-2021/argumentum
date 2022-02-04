4304f7708a096d7633dfb91cc95b9f05fb89fdbe
tane-git <tanelives@gmail.com>
2022-02-04 16:20:18 +1300

    refactor last commited test

--- server/routes/arguments.test.js
+++ server/routes/arguments.test.js
@@ -4,71 +4,6 @@ const db = require('../db/arguments')
 
 jest.mock('../db/arguments')
 
-// const mockArguments = {
-//   stupid: [
-//     {
-//       id: 5,
-//       category: 'stupid',
-//       name: 'Freewill',
-//       description: 'Do we have it?',
-//       side0: 'Yes',
-//       side1: 'No'
-//     },
-//     {
-//       id: 6,
-//       category: 'stupid',
-//       name: 'The nature of reality',
-//       description: 'Do we live in a simulation?',
-//       side0: 'Yes',
-//       side1: 'No'
-//     }
-//   ],
-//   serious: [
-//     {
-//       id: 1,
-//       category: 'serious',
-//       name: 'Aliens',
-//       description: 'Do they exist?',
-//       side0: 'Yes',
-//       side1: 'No'
-//     },
-//     {
-//       id: 2,
-//       category: 'serious',
-//       name: 'Vaccines',
-//       description: 'Are they harmful?',
-//       side0: 'Yes',
-//       side1: 'No'
-//     },
-//     {
-//       id: 7,
-//       category: 'serious',
-//       name: 'Vacine Mandates',
-//       description: 'Should we have them?',
-//       side0: 'For',
-//       side1: 'Against'
-//     }
-//   ],
-//   fun: [
-//     {
-//       id: 3,
-//       category: 'fun',
-//       name: 'Dogs Vs Cats',
-//       description: 'Which is the best pet?',
-//       side0: 'Dogs',
-//       side1: 'Cats'
-//     },
-//     {
-//       id: 4,
-//       category: 'fun',
-//       name: 'Batman Vs Superman',
-//       description: 'Who would win in a fight?',
-//       side0: 'Batman',
-//       side1: 'Superman'
-//     }
-//   ]
-// }
-
 const mockArguments = [
   { id: 2, category: 'stupid', name: 'Are we in a simulation?', description: '', side0: 'Yes', side1: 'No' },
   { id: 1, category: 'stupid', name: 'God', description: '', side0: 'Yes', side1: 'No' },
@@ -106,8 +41,6 @@ describe('GET /api/v1/args', () => {
       .get('/api/v1/args')
       .expect(200)
       .then((res) => {
-      // .then((err, res) => {
-        // expect(err).toBeNull()
         expect(res.body).toEqual(
           expect.objectContaining({
             stupid: expect.any(Array),
